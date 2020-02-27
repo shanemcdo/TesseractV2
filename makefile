bbdf5cc6d003724625c7941e7ac09d8ecb53c6ab
Shane McDonough <shanemcdman@gmail.com>
2020-02-26 23:16:02 -0800

    Basic openGL setup

--- makefile
+++ makefile
@@ -1,9 +1,9 @@
 all: main.o 
-	g++ bin/*.o -o bin/test.exe -I include
+	g++ bin/*.o -o bin/test.exe -I include -lopenGL32 -lfreeGLUT 
 	@echo
 
 main.o: src/main.cpp
-	g++ src/main.cpp -c -o bin/main.o -I include
+	g++ src/main.cpp -c -o bin/main.o -I include -lopenGL32 -lfreeGLUT 
 
 clean:
 	rm bin/*
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
 #include<iostream>
+#include<gl/GLUT.h>
 using namespace std;
 
-int main(){
-	cout << "Hello World!" << endl;
+void display(){
+	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );//I do as the sample code says
+
+	glutSwapBuffers();//I do as sample code commands
+}
+
+void kbin(unsigned char key, int x, int y){
+}
+
+int main(int argc, char* argv[]){
+	//initialize
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition ( 0, 0 ) ;
+	glutInitWindowSize ( 675, 675 ) ;
+	glutCreateWindow( "Tessaract" );
+	glOrtho(-12, 12, -12, 12, 12, -12);
+	glRotatef(45, -1, 1, 0);
+	
+	//functions
+	glutDisplayFunc(display);
+	glutKeyboardFunc(kbin);
+	glutIdleFunc(display);
+
+	//loop
+	glutMainLoop();
+
 	return 0;
 }

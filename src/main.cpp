20a57740d94ed58d5fa83ef3fd4688688034cd8e
Shane McDonough <shanemcdman@gmail.com>
2020-02-29 02:11:17 -0800

    Add keyboard camera controls

--- src/main.cpp
+++ src/main.cpp
@@ -24,11 +24,12 @@ coord c[16]= {
 	{50, -50, -50, -50}, 
 };
 
+double speed = 0.001;
+
 void display(){
 	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );//I do as the sample code says
 	
-	glRotatef(0.01, 0, 1, 0);
-	rotate(c, 0.001);
+	rotate(c, speed);
 	project(c);
 
 	glutSwapBuffers();//I do as sample code commands
@@ -37,6 +38,18 @@ void display(){
 void kbin(unsigned char key, int x, int y){
 	if(key == '\x1b')//esc key
 		exit(0);
+	else if(key == 'w')//w
+		glRotatef(-1, 1, 0, 0);
+	else if(key == 'a')//a
+		glRotatef(-1, 0, 1, 0);
+	else if(key == 's')//s
+		glRotatef(1, 1, 0, 0);
+	else if(key == 'd')//d
+		glRotatef(1, 0, 1, 0);
+	else if(key == 'q')//q
+		glRotatef(1, 0, 0, 1);
+	else if(key == 'e')//e
+		glRotatef(-1, 0, 0, 1);
 }
 
 int main(int argc, char* argv[]){
